6261bd4e9e73a4fa5778c8a3546d51f1ac650738
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2022-01-19 11:17:33 -0800

    add blog example using Redux

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <section>
+      <h1>404!</h1>
+      <p><Link to="/">Go to home page</Link></p>
+    </section>
+  );
+}
+
+export default NotFound;
