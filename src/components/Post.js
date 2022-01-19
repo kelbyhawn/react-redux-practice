ae5fff2a71d0b5f03cf193c5966b7febd26bffea
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2022-01-19 15:50:10 -0800

    add console.log(post)

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Post = ({ post, excerpt }) => (
-  <article className={excerpt ? "post-excerpt" : "post"}>
-    <h2>{post.title}</h2>
-    <p>{excerpt ? post.body.substring(0, 100) : post.body}</p>
+export const Post = ({ post, excerpt }) => {
+  console.log(post);
+  return (
+    <article className={excerpt ? "post-excerpt" : "post"}>
+      <h2>{post.title}</h2>
+      <p>{excerpt ? post.body.substring(0, 100) : post.body}</p>
 
-    {excerpt && (
-      <Link to={`/posts/${post.id}`} className="button">
-        View Post
-      </Link>
-    )}
-  </article>
-)
+      {excerpt && (
+        <Link to={`/posts/${post.id}`} className="button">
+          View Post
+        </Link>
+      )}
+    </article>
+  )
+}
