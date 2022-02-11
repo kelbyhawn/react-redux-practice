a808cc5752db0b7b257c6a13d9a43122a723b9e0
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2022-02-11 12:00:28 -0800

    fix path to single post page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import PostsPage from "./pages/PostsPage";
-import SinglePostPage from './pages/SinglePostPage';
+import SinglePostPage from "./pages/SinglePostPage";
 import NotFound from "./pages/NotFound";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <Router>
+      <NavBar />
       <Routes>
-        <Route exact path="/" element={<DashboardPage />} />
-        <Route exact path="posts" element={<PostsPage />}>
-          <Route exact path=":id" element={<SinglePostPage />} />
-        </Route>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="posts" element={<PostsPage />} />
+        <Route path="/posts/:id" element={<SinglePostPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>  
